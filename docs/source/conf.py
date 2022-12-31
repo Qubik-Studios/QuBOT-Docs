1bd9800b6b52f8175103a50fbfd1b9b16611f85c
ConmineLP <40390857+ConmineLP@users.noreply.github.com>
2022-12-31 15:14:08 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'QuBOT Engine'
+copyright = '2023, Qubik Studios'
+author = 'Qubik Studios'
 
 release = '0.1'
 version = '0.1.0'
